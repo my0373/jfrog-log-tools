596d2415a68e385d6ab5fba4da8a9e7a7f0a30bd
Matt York <my0373@gmail.com>
2024-09-21 13:49:09 +0100

    Initial branch

--- log_anonymiser.py
+++ log_anonymiser.py
@@ -1,36 +1,28 @@
-"""Artifactory Log Anonymiser
-    
-This script exists to read a source log file and create anonymised output for support purposes.
-
-To show the available options, please run
-
-log_anonymiser.py -h
-
-"""
 from datetime import datetime
 import os
 import argparse
+import sys
 
 def parse_arguments():
     """
     Parse command line arguments.
     """
     parser = argparse.ArgumentParser()
-    
-    # Add the -f option to specify the log file. If omitted, then stdout is used.
-    parser.add_argument("-o", 
-                        "--outputfile",
-                        help="specify the output file",
+
+    # Add the -o option to specify the output directory.
+    parser.add_argument("-o",
+                        "--outputdir",
+                        help="Specify the output directory",
                         type=str,
                         required=True)
-    
-    # Add the -t option to optional pattern argument.
-    parser.add_argument("-i", 
-                        "--input", 
-                        help="The source input file",
+
+    # Add the -i option to specify the input file or directory.
+    parser.add_argument("-i",
+                        "--input",
+                        help="The source input file or directory",
                         type=str,
                         required=True)
-    
+
     parser.add_argument("-d",
                         "--debug",
                         help="Enable debug mode",
@@ -38,130 +30,144 @@ def parse_arguments():
                         required=False)
 
     return parser.parse_args()
-  
+
 def get_logfile_version(logfile):
     """
     Get the version of the log file.
     """
-    with open(logfile, "r") as log:
-
-        first_line = log.readline()
-        if validate_v6_line(first_line):
-            return "v6"
-        elif validate_v7in_line(first_line):
-            return "v7in"
-        else:
-            return "unknown"
-        
-        
+    try:
+        with open(logfile, "r") as log:
+            first_line = log.readline()
+            if validate_v6_line(first_line):
+                return "v6"
+            elif validate_v7in_line(first_line):
+                return "v7in"
+            else:
+                return "unknown"
+    except Exception as e:
+        print(f"Error reading {logfile}: {e}")
+        return "error"
 
 def validate_v6_line(first_line):
     """
-    Validate the first line of a log file to check the version
+    Validate the first line of a log file to check the version.
     """
-    # Get the date string from the first line
-    date_str =  first_line.split("|")[0]
+    date_str = first_line.split("|")[0]
 
-    # test if the date string is in the correct format for a v6 file.
     try:
         # Set the expected string format
         date_format = '%Y%m%d%H%M%S'
-
-        date_obj = datetime.strptime(date_str, date_format)
-    except ValueError:
-        return False
-    
-    return True 
-
-def validate_v7in_line(first_line):
-    timefield =  first_line.split("|")[0]
-
-    try:
-
-        # Example with the standard date and time format
-        date_format = '%Y-%m-%d %H:%M:%S'
-
-        date_obj = datetime.fromisoformat(timefield)
-        print(date_obj)
-
-        # Example with a different format
-
-        date_str = '02/28/2023 02:30 PM'
-        date_format = '%m/%d/%Y %I:%M %p'
-
-        date_obj = datetime.strptime(date_str, date_format)
+        datetime.strptime(date_str, date_format)
         return True
     except ValueError:
         return False
 
+def validate_v7in_line(first_line):
+    """
+    Validate the first line of a v7in log file.
+    """
+    timefield = first_line.split("|")[0]
 
-def anonymise_v6_logfile(input_logfile, output_logfile):
+    try:
+        # Standard ISO datetime format
+        datetime.fromisoformat(timefield)
+        return True
+    except ValueError:
+        return False
+
+def anonymise_v6_logfile(input_logfile, output_logfile, debug=False):
+    """
+    Anonymize v6 log files.
+    """
     with open(input_logfile, "r") as infile, open(output_logfile, "w") as outfile:
         for line in infile:
             tmpline = line.split("|")
-            timestamp = tmpline[0]
-            request_duration = tmpline[1]
-            request_type = tmpline[2]
-            remote_address = "ADDRESS_REDACTED"
-            username = "USER_REDACTED"
-            request_method = tmpline[5]
-            request_url = "URL_REDACTED"
-            protocol_version = tmpline[7]
-            response_code = tmpline[8]
-            request_content_length = tmpline[9]
-            
-            anonymised_line = f"{timestamp}|{request_duration}|{request_type}|{remote_address}|{username}|{request_method}|{request_url}|{protocol_version}|{response_code}|{request_content_length}\n".strip()
-            outfile.write(anonymised_line + '\n')
-        
 
+            # Check if the line has enough fields
+            if len(tmpline) < 10:
+                if debug:
+                    print(f"Skipping malformed line (v6): {line.strip()}")
+                continue
 
-def anonymise_v7in_logfile(input_logfile, output_logfile):
+            anonymised_line = f"{tmpline[0]}|{tmpline[1]}|{tmpline[2]}|ADDRESS_REDACTED|USER_REDACTED|{tmpline[5]}|URL_REDACTED|{tmpline[7]}|{tmpline[8]}|{tmpline[9]}\n"
+            outfile.write(anonymised_line)
+
+            if debug:
+                print(f"Anonymised v6 log line: {anonymised_line.strip()}")
+
+def anonymise_v7in_logfile(input_logfile, output_logfile, debug=False):
+    """
+    Anonymize v7in log files.
+    """
     with open(input_logfile, "r") as infile, open(output_logfile, "w") as outfile:
         for line in infile:
             tmpline = line.split("|")
-            timestamp = tmpline[0]
-            request_traceid = tmpline[1]
-            remote_address = "ADDRESS_REDACTED"
-            username = "USER_REDACTED"
-            request_method = tmpline[4]
-            request_url = "URL_REDACTED"
-            return_status = tmpline[6]
-            request_content_length = tmpline[7]
-            response_content_length = tmpline[8]
-            request_duration = tmpline[9]
-            request_user_agent = tmpline[10]
-            
-            anonymised_line = f"{timestamp}|{request_traceid}|{remote_address}|{username}|{request_method}|{request_url}|{return_status}|{request_content_length}|{response_content_length}|{request_duration}|{request_user_agent}"
-    
-            outfile.write(anonymised_line.strip() + '\n')
-        
 
-def anonymise_v7out_logfile(input_logfile, output_logfile):
-    #TODO: Implement this function
+            # Check if the line has enough fields
+            if len(tmpline) < 11:
+                if debug:
+                    print(f"Skipping malformed line (v7in): {line.strip()}")
+                continue
+
+            anonymised_line = f"{tmpline[0]}|{tmpline[1]}|ADDRESS_REDACTED|USER_REDACTED|{tmpline[4]}|URL_REDACTED|{tmpline[6]}|{tmpline[7]}|{tmpline[8]}|{tmpline[9]}|{tmpline[10]}\n"
+            outfile.write(anonymised_line)
+
+            if debug:
+                print(f"Anonymised v7in log line: {anonymised_line.strip()}")
+
+def anonymise_v7out_logfile(input_logfile, output_logfile, debug=False):
+    """
+    Placeholder function for v7out log anonymization.
+    """
     pass
 
-def main():
+def process_log_file(input_file, output_dir, unprocessed_files, debug=False):
+    """
+    Process and anonymize a single log file.
+    """
+    logfile_version = get_logfile_version(input_file)
 
-    args = parse_arguments()
-
-    logfile_version = get_logfile_version(args.input)
+    # Generate the output file path based on the input file name.
+    output_file = os.path.join(output_dir, os.path.basename(input_file))
 
     if logfile_version == "v6":
-        print("v6")
-        anonymise_v6_logfile(args.input, args.outputfile)
+        print(f"Processing v6 log file: {input_file}")
+        anonymise_v6_logfile(input_file, output_file, debug)
+    elif logfile_version == "v7in":
+        print(f"Processing v7in log file: {input_file}")
+        anonymise_v7in_logfile(input_file, output_file, debug)
+    elif logfile_version == "v7out":
+        print(f"Processing v7out log file: {input_file}")
+        anonymise_v7out_logfile(input_file, output_file, debug)
+    elif logfile_version == "unknown" or logfile_version == "error":
+        print(f"Unknown or error in log file version: {input_file}")
+        unprocessed_files.append(input_file)
 
-    if logfile_version == "v7in":
-        print("v7in")
-        anonymise_v7in_logfile(args.input, args.outputfile)
-    
-    if logfile_version == "v7out":
-        print("v7out")
-        anonymise_v7out_logfile(args.input, args.outputfile)
+def main():
+    args = parse_arguments()
 
+    # List to keep track of files with unknown or unprocessable versions.
+    unprocessed_files = []
+
+    # Check if input is a directory or a single file.
+    if os.path.isdir(args.input):
+        input_files = [os.path.join(args.input, f) for f in os.listdir(args.input) if os.path.isfile(os.path.join(args.input, f))]
     else:
-        print(f"Unknown log file version: {args.input}")
-        sys.exit(1)
-    
-    
+        input_files = [args.input]
+
+    # Ensure output directory exists.
+    if not os.path.exists(args.outputdir):
+        os.makedirs(args.outputdir)
+
+    # Process each input file.
+    for input_file in input_files:
+        process_log_file(input_file, args.outputdir, unprocessed_files, args.debug)
+
+    # Output list of files that couldn't be processed.
+    if unprocessed_files:
+        print("\nThe following log files could not be processed due to unknown version or errors:")
+        for file in unprocessed_files:
+            print(f"  - {file}")
+
 if __name__ == '__main__':
     main()
